95d707de70849149723a4ed3a66f8cbc8152a88c
onarman <onarmannn@gmail.com>
2022-06-30 20:03:15 +0300

    smt added

---
+++ React/projects/002-Language-Cards-RP-02/solution/src/components/item/Item.js
@@ -0,0 +1,31 @@
+import "./Item.css";
+import {useState} from "react";
+
+const Item = ({card}) => {
+    console.log(card);
+    const [showLogo,setShowLogo] = useState(true);
+
+    const {name,img, options} =card;
+
+  return (
+    <div className="card" onClick={() => setShowLogo(!showLogo)} >
+        {showLogo ? (
+            <div>
+            <img className="card-logo" src={img} alt="card" />
+            <h3 className="card-title" >{name}</h3>
+        </div>
+        ):(
+            <ul className="list">
+            {options.map((element,index)=> {
+                return <li key={index} >{element}</li>
+            })}
+        </ul>
+        )}
+        
+       
+        
+    </div>
+  )
+}
+
+export default Item
--- React/rc06_forms_otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
+++ React/rc06_forms_otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
@@ -1,25 +1,55 @@
-import { useState } from 'react';
+import { useState } from "react"
 
-const KeyboardClipboard = () => {
-  const [inputValue, setInputValue] = useState('');
 
-  const handleKeyDown = (e) => {
-    // console.log(e.keyCode);
-    e.target.value = e.target.value.toUpperCase();
-    e.keyCode === 13 && alert('Entera basildi');
-  };
+const KeyboardClipboard = (e) => {
+  const [inputValue,setInputValue] = useState("");
+
+const handleKeyDown = (e) => {
+  // console.log(e.keyCode)
+  e.target.value = e.target.value.toUpperCase()
+  e.keyCode === 13 && alert('entered')
+
+}
+
+const handleAreaPaste = (e) => {
+  e.target.value += e.clipboardData.getData('text').toLowerCase();
+  e.target.style.border = "3px solid red";
+  e.target.style.backgroundColor = "pink";
+
+  e.preventdefault();
+}
+
+const handleAreaChange = (e) => {
+  if(!e.target.value){
+    e.target.style.border = "3px solid red";
+  e.target.style.backgroundColor = "purple";
+  }
+}
+
   return (
-    <div className="container text-center">
-      <h2>CLIPBOARD EVENTS </h2>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      <p className="text-start m-4">{inputValue}</p>
-    </div>
-  );
-};
 
-export default KeyboardClipboard;
+    <div className="container text-center">
+      <h2>Clipboard Events</h2>
+      <input 
+      type="text" 
+      name="" 
+      id="" 
+      value={inputValue} 
+      onChange={(e) => setInputValue(e.target.value)}
+      onKeyDown ={handleKeyDown}/>
+
+      <p>{inputValue}</p>
+
+      <textarea 
+      name="area" 
+      id="area" 
+      cols="50" 
+      rows="10" 
+      onPaste={handleAreaPaste} 
+      onChange={handleAreaChange}></textarea>
+
+    </div>
+  )
+}
+
+export default KeyboardClipboard
